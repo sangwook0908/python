9a1fce2f739342b5a14b15df773c1d420f09e0c5
sangwook0908 <99239273+sangwook0908@users.noreply.github.com>
2022-02-22 14:30:15 +0900

    Add files via upload

---
+++ 20220108_x y z munja aaa 값과 타입.py
@@ -0,0 +1,19 @@
+# x y z munja aaa 값과 타입
+x = 100
+y = 500
+z = 3.33
+munja = 'a'
+aaa = True
+
+print (x)
+print ("%d" %x)
+print (y)
+print (z)
+print (munja)
+print (aaa)
+print()
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(munja))
+print(type(aaa))
